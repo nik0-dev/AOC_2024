d15a3c60c5ce291fcd90010fe83e51b9653ef3b5
Isabelle <121070808+nik0-dev@users.noreply.github.com>
2024-12-02 18:37:42 -0600

    Update part1.rs

--- day2/part1.rs
+++ day2/part1.rs
@@ -1,26 +1,3 @@
-pub fn is_safe_level(vec: Vec<i32>) -> bool {
-    let mut iter = vec.iter();
-    let mut sign : i32 = 0;
-
-    let mut last : i32 = match iter.next() {
-        Some(val) => *val,
-        None => return false
-    };
-
-    for num in iter {            
-        let updated_sign : i32 = if last <= *num { 1 } else { -1 };
-        let diff : u32 = num.abs_diff(last);
-        
-        let invalid_level_adjustment : bool = sign != 0 && sign != updated_sign || diff < 1 || diff > 3;
-        if invalid_level_adjustment { return false; } 
-
-        sign = updated_sign;
-        last = *num;
-    }
-    
-    return true;
-}
-
 pub fn part_one(input: &str) -> Option<u32> {
     let mut output : u32 = 0;
     
@@ -29,7 +6,8 @@ pub fn part_one(input: &str) -> Option<u32> {
         let mut nums : Vec<i32> = Vec::new();
 
         for token in tokens { nums.push(token.parse().expect("Invalid Token.")); }
-        
+
+        // defined in helpers.rs
         if is_safe_level(nums) { output += 1; }
     }
 
