0432ee3e2bfef11ba3c724d8ee935b54377c09d7
Isabelle <121070808+nik0-dev@users.noreply.github.com>
2024-12-02 18:35:27 -0600

    Update part1.rs

--- day2/part1.rs
+++ day2/part1.rs
@@ -1,30 +1,36 @@
+pub fn is_safe_level(vec: Vec<i32>) -> bool {
+    let mut iter = vec.iter();
+    let mut sign : i32 = 0;
+
+    let mut last : i32 = match iter.next() {
+        Some(val) => *val,
+        None => return false
+    };
+
+    for num in iter {            
+        let updated_sign : i32 = if last <= *num { 1 } else { -1 };
+        let diff : u32 = num.abs_diff(last);
+        
+        let invalid_level_adjustment : bool = sign != 0 && sign != updated_sign || diff < 1 || diff > 3;
+        if invalid_level_adjustment { return false; } 
+
+        sign = updated_sign;
+        last = *num;
+    }
+    
+    return true;
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
     let mut output : u32 = 0;
-
+    
     for line in input.split("\n") {
-        let mut tokens = line.split_whitespace();
+        let tokens = line.split_whitespace();
+        let mut nums : Vec<i32> = Vec::new();
 
-        let mut sign : i32 = 0;
-        let mut valid_level : bool = true;
-
-        let mut last : i32 = match tokens.next() {
-            Some(token) => token.parse::<i32>().expect("Invalid Token."), 
-            None => break
-        };
-
-        for token in tokens {
-            let num : i32 = token.parse().expect("Invalid Token.");
-            
-            let updated_sign : i32 = if last <= num { 1 } else { -1 };
-            let diff : u32 = num.abs_diff(last);
-
-            if sign != 0 && sign != updated_sign || diff < 1 || diff > 3 { valid_level = false; break; }
-
-            sign = updated_sign;
-            last = num;
-        }
-
-        if valid_level { output += 1; }
+        for token in tokens { nums.push(token.parse().expect("Invalid Token.")); }
+        
+        if is_safe_level(nums) { output += 1; }
     }
 
     Some(output)
