0a40dd02863aa8ba65c062204f89d173fff9ea19
Isabelle <121070808+nik0-dev@users.noreply.github.com>
2024-12-03 07:53:18 -0600

    Update part1.rs

--- day3/part1.rs
+++ day3/part1.rs
@@ -1,13 +1,16 @@
 pub fn part_one(input: &str) -> Option<u32> {
     use regex::Regex;
 
+    // get subcaptures for "mul(x, y)" of "x" and "y"
     let expr = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
 
     let output = expr.captures_iter(input)
         .map(|caps| 
         { 
+            // discard overall match, assign the str slice captures to variables named op1 + op2
             let (_, [op1, op2]) = caps.extract();
-            op1.parse::<u32>().expect("Invalid Token") * op2.parse::<u32>().expect("Invalid Token")
+            // parse and return the product to the map
+            op1.parse::<u32>().unwrap() * op2.parse::<u32>().unwrap
         })
         .collect::<Vec<u32>>()
         .iter()
